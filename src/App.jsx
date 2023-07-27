34eb31e1f5c336c6831046cf25bd30d188432f94
ChrisGlz14 <christiang14@hotmail.com.ar>
2023-07-27 17:50:03 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import ContadorComponent from "./components/ContadorComponent";
+
+function App() {
+ 
+  return (
+    <>
+      <ContadorComponent />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ContadorComponent.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useCounter } from '../Hooks/usecounter'
+
+function ContadorComponent() {
+
+  const {counter, increment,decrement,reset} =  useCounter(0)
+
+  return (
+    <>
+    <h2 className="text-center">Aplicacion con Hooks</h2>
+      <hr />  
+    <h1>Contador: {counter}</h1>
+      <button className="btn btn-primary" onClick={()=> increment(1)}>sumar</button>
+      <button className="btn btn-danger m-4" onClick={()=> reset()}>resetear</button>
+      <button className="btn btn-primary" onClick={()=> decrement(4, false)}>Restar</button>
+    </>
+  )
+}
+
+export default ContadorComponent
